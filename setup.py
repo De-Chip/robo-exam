b3909d1ff803acd3ee190d5efa54e516c92720d7
De-Chip <97140105+De-Chip@users.noreply.github.com>
2022-11-29 21:33:10 +0900

    기본 setup.py 업로드
    기본 파일 공유합니다.

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ package_name = 'robotics-exam-drive'
 setup(
     name=package_name,
     version='0.1.0',
-    packages=find_packages(exclude=['te
+    packages=find_packages(exclude=['test']),
+    data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
